b3573ae8fb808010bc0289ef2e7ebf737d6ff89c
imdreamer <743295483@qq.com>
2020-09-02 13:10:13 +0800

    [yangqian] fix: should get object prototype for an object.

--- __tests__/pieceOfCake/object_spec.js
+++ __tests__/pieceOfCake/object_spec.js
@@ -153,7 +153,7 @@ describe('for object', () => {
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = Object.prototype;
     // --end->
 
     expect(Object.getPrototypeOf(emptyObject)).toBe(expected);
@@ -164,7 +164,7 @@ describe('for object', () => {
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = null;
     // --end->
 
     expect(Object.getPrototypeOf(objectPrototype)).toEqual(expected);
